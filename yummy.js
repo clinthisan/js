236745afa674676e47e8e59f46c0d2e5f6dcc129
clinthisan <91356977+clinthisan@users.noreply.github.com>
2021-09-25 18:20:30 +0700

    Update yummy.js

--- yummy.js
+++ yummy.js
@@ -1,15 +1,25 @@
+//<![CDATA[
+
     var go_current      = window.location.href;
-    var reff      = document.referrer;
+    var reff            = document.referrer;
+    
+
     function rChoice(arr) {
-     return arr[Math.floor(arr.length * Math.random())];
- }
+        return arr[Math.floor(arr.length * Math.random())];
+    }
+
     var direct_link_ads = rChoice([
-                            "",
+                            "https://harmlesstacticalhonorable.com/bcd48n8axe?key=1f9368f48204718999a16702a66cd2b0",
                         ]);
+
     var ars             = rChoice([
                             "https://yummy.hickoryplano.com",
                         ]);
-    var dir_type        = "arsae"; //refresh, domain, path, arsae
+
+
+    var dir_type        = 'arsae'; // refresh, domain, path, arsae
+
+
     if(dir_type == 'refresh')
     {
         //REFRESH
@@ -31,3 +41,48 @@
         //==> ARSAE DIRECT
         go_current            = ars + '/?arsae='+ encodeURIComponent(go_current) + '&arsae_ref='+ encodeURIComponent(reff);
     }
+
+    $(document).ready(function()
+    {
+        $(document.body).append(popbox);
+
+        if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s)))
+        {
+            $(window).scroll(function (event) {
+                var scroll = $(window).scrollTop();
+                if (scroll >= 200) {
+                    $('#popbox').removeClass('hide');
+                }
+                console.log('scroll..');                    
+            });
+        }
+
+        $(document).on('click','.g_url',function(e)
+        {
+            e.preventDefault();            
+
+            window.open(go_current,"_blank");
+          
+            window.location.href = direct_link_ads;        
+        });
+
+        $("[id*='google-cache']").remove();        
+
+        $(document).on('submit','#search-box',function(e){
+            e.preventDefault();
+            var query = $('input[name="q"]').val();
+            query = query.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '').replace(/\s\s+/g, ' ');
+            var target = 'site:'+location.host+' '+query;
+            var uri= 'https://www.google.com/search?q='+encodeURIComponent(target);
+            window.open(uri, '_blank');
+        });
+
+        $(document).on('click','.ads-img',function(e)
+        {
+            e.preventDefault();
+            window.open(go_ads, '_blank');
+        });
+
+    });
+
+//]]>
